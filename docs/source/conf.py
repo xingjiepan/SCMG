6abb0bacd42faf3e424f18c4dbb948f16ba02a37
Xingjie Pan <xingjiepan@gmail.com>
2025-04-18 14:11:04 -0400

    Update the configuration for building docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 # -- Project information
 
 project = 'SCMG'
